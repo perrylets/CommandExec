1c8bc9b181052722bd84bc5a41be5468801b1e20
Perry <perry@perry.dev.br>
2023-08-24 00:54:57 -0300

    Remove .exe for powershell on Command.Shell

--- CommandExec/Command.cs
+++ CommandExec/Command.cs
@@ -91,7 +91,7 @@ namespace CommandExec
     /// <returns>The command used to run the shell.</returns>
     public static Command Shell(params string[] args)
     {
-      (string shellCommand, string shellArg) = isUnix ? ("bash", "-c") : ("powershell.exe", string.Empty);
+      (string shellCommand, string shellArg) = isUnix ? ("bash", "-c") : ("powershell", string.Empty);
       Command shell = new Command(shellCommand)
         .AddArg(shellArg);
 
